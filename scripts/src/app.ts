c91d1255534181590ce6b9e4b5d5d850ae15eba4
pvczku <patryk.kulma.dev@gmail.com>
2023-06-19 23:39:16 +0200

    :zap: first try with the data

--- scripts/src/app.ts
+++ scripts/src/app.ts
@@ -1,3 +1,31 @@
-export default function app() {
-  console.log("app");
+class Average {
+  data: [];
+  constructor() {
+    this.data = [];
+    this.getData();
+  }
+
+  ping() {
+    console.log("pong");
+  }
+  getData() {
+    const row = document.getElementById("przedmioty_86613");
+    console.log(row?.children[0]?.children[0]?.children[1]?.children.length);
+    const tbody = row?.children[0]?.children[0]?.children[1]?.children;
+    for (let i = 0; i < tbody!.length; i++) {
+      if (
+        tbody![i]?.classList.contains("detail-grades") &&
+        tbody![i]?.children[2]?.innerHTML !== "przewidywana roczna" &&
+        tbody![i]?.children[2]?.innerHTML !== "roczna" &&
+        tbody![i]?.children[2]?.innerHTML !== "przewidywana śródroczna" &&
+        tbody![i]?.children[2]?.innerHTML !== "śródroczna" &&
+        tbody![i]?.children[2]?.innerHTML !== "nieprzygotowanie"
+      ) {
+        console.log(tbody![i]); // TODO: do next
+      }
+    }
+  }
 }
+
+let average;
+!average ? (average = new Average()) : null;
