2aa38cd63f178d611b9f6b8a4c7880dcd867e0a7
pvczku <patryk.kulma.dev@gmail.com>
2023-06-20 16:23:48 +0200

    :zap: add regular grades data scraper
    added interface Grade and Subject, class Grades, a method getRegularGrades, which  gets regular grades from the website and stores it in an object and formatSubjectName method which formats the name of the subject, so there is no whitespaces etc.

--- scripts/src/app.ts
+++ scripts/src/app.ts
@@ -1,31 +1,81 @@
-class Average {
-  data: [];
+interface Grade {
+  value: number;
+  weight: number;
+}
+
+interface Subject {
+  name: string;
+  grades: [Grade[], Grade[]];
+}
+
+class Grades {
+  regularGrades: { [key: string]: Subject };
+  regularGradesTable: HTMLCollection;
+  pointGrades: any;
+  pointGradesTable: HTMLCollection;
+
   constructor() {
-    this.data = [];
-    this.getData();
+    this.regularGrades = {};
+    this.pointGrades = {};
+    this.regularGradesTable = document.getElementsByClassName("decorated stretch")[1]!.children[1]!.children;
+    this.pointGradesTable = document.getElementsByClassName("decorated stretch")[2]!.children[1]!.children;
+    this.getRegularGrades();
   }
 
   ping() {
-    console.log("pong");
+    console.log(document.getElementsByClassName("decorated stretch")[1]?.children[1]?.children);
   }
-  getData() {
-    const row = document.getElementById("przedmioty_86613");
-    console.log(row?.children[0]?.children[0]?.children[1]?.children.length);
-    const tbody = row?.children[0]?.children[0]?.children[1]?.children;
-    for (let i = 0; i < tbody!.length; i++) {
-      if (
-        tbody![i]?.classList.contains("detail-grades") &&
-        tbody![i]?.children[2]?.innerHTML !== "przewidywana roczna" &&
-        tbody![i]?.children[2]?.innerHTML !== "roczna" &&
-        tbody![i]?.children[2]?.innerHTML !== "przewidywana śródroczna" &&
-        tbody![i]?.children[2]?.innerHTML !== "śródroczna" &&
-        tbody![i]?.children[2]?.innerHTML !== "nieprzygotowanie"
-      ) {
-        console.log(tbody![i]); // TODO: do next
+
+  getRegularGrades() {
+    console.log(this.regularGradesTable);
+    for (let i = 0; i < this.regularGradesTable!.length - 2; i += 2) {
+      const subject = this.regularGradesTable![i]!.children[1]!.innerHTML;
+      const formattedSubject = this.formatSubjectName(this.regularGradesTable![i]!.children[1]!.innerHTML);
+      console.log(subject);
+      if (this.regularGradesTable![i]!.children[2]!.innerHTML !== "Brak ocen") {
+        const grades = this.regularGradesTable![i + 1]!.children[0]!.children[0]!.children[1]!.children;
+        console.log(grades);
+        let semester = -1;
+        let gradeArray: [Grade[], Grade[]] = [[], []];
+        for (let j = 0; j < grades.length; j++) {
+          if (grades[j]!.innerHTML.includes("Okres")) {
+            semester++;
+          } else {
+            console.log(grades[j]?.children[0], semester);
+            console.log(grades[j]?.children[6]?.innerHTML !== "");
+            if (grades[j]?.children[6]?.innerHTML !== "") {
+              const grade =
+                grades[j]?.children[0]?.innerHTML!.length! > 1
+                  ? parseFloat(grades[j]?.children[0]?.innerHTML[0]!) + 0.5
+                  : parseFloat(grades[j]?.children[0]?.innerHTML[0]!);
+              console.log(grade);
+              const weight = parseFloat(grades[j]?.children[6]?.innerHTML!);
+              semester === 0
+                ? gradeArray[0]!.push({ value: grade, weight: weight })
+                : gradeArray[1]!.push({ value: grade, weight: weight });
+            }
+          }
+        }
+        this.regularGrades[formattedSubject] = { name: subject, grades: gradeArray };
+      } else {
+        console.log("brak ocen lub ocena punktowa");
       }
     }
+    console.log(this.regularGrades["język_angielski"]);
+  }
+
+  getPointGrades() {}
+
+  formatSubjectName(name: string) {
+    let formattedName = "";
+    if (name.search("<") !== -1) {
+      formattedName = name.split("<")[0]!.trim().replaceAll(" ", "_").replaceAll(".", "").toLowerCase();
+    } else {
+      formattedName = name.trim().replaceAll(" ", "_").replaceAll(".", "").toLowerCase();
+    }
+    return formattedName;
   }
 }
 
 let average;
-!average ? (average = new Average()) : null;
+!average ? (average = new Grades()) : null;
