acd626f929aef9e393138ee785561c0c0203e929
pvczku <patryk.kulma.dev@gmail.com>
2023-06-20 17:53:10 +0200

    :zap: add point grades data scraper
    added a method getPointGrades, which gets point grades from the website and stores it in an object

--- scripts/src/app.ts
+++ scripts/src/app.ts
@@ -11,7 +11,7 @@ interface Subject {
 class Grades {
   regularGrades: { [key: string]: Subject };
   regularGradesTable: HTMLCollection;
-  pointGrades: any;
+  pointGrades: { [key: string]: Subject };
   pointGradesTable: HTMLCollection;
 
   constructor() {
@@ -20,35 +20,33 @@ class Grades {
     this.regularGradesTable = document.getElementsByClassName("decorated stretch")[1]!.children[1]!.children;
     this.pointGradesTable = document.getElementsByClassName("decorated stretch")[2]!.children[1]!.children;
     this.getRegularGrades();
+    this.getPointGrades();
   }
 
   ping() {
-    console.log(document.getElementsByClassName("decorated stretch")[1]?.children[1]?.children);
+    console.log("pong");
   }
 
   getRegularGrades() {
-    console.log(this.regularGradesTable);
     for (let i = 0; i < this.regularGradesTable!.length - 2; i += 2) {
-      const subject = this.regularGradesTable![i]!.children[1]!.innerHTML;
+      const subject =
+        this.regularGradesTable![i]!.children[1]!.innerHTML.search("<") != -1
+          ? this.regularGradesTable![i]!.children[1]!.innerHTML.split("<")[0]!.trim()
+          : this.regularGradesTable![i]!.children[1]!.innerHTML!;
       const formattedSubject = this.formatSubjectName(this.regularGradesTable![i]!.children[1]!.innerHTML);
-      console.log(subject);
       if (this.regularGradesTable![i]!.children[2]!.innerHTML !== "Brak ocen") {
         const grades = this.regularGradesTable![i + 1]!.children[0]!.children[0]!.children[1]!.children;
-        console.log(grades);
         let semester = -1;
         let gradeArray: [Grade[], Grade[]] = [[], []];
         for (let j = 0; j < grades.length; j++) {
           if (grades[j]!.innerHTML.includes("Okres")) {
             semester++;
           } else {
-            console.log(grades[j]?.children[0], semester);
-            console.log(grades[j]?.children[6]?.innerHTML !== "");
             if (grades[j]?.children[6]?.innerHTML !== "") {
               const grade =
                 grades[j]?.children[0]?.innerHTML!.length! > 1
                   ? parseFloat(grades[j]?.children[0]?.innerHTML[0]!) + 0.5
                   : parseFloat(grades[j]?.children[0]?.innerHTML[0]!);
-              console.log(grade);
               const weight = parseFloat(grades[j]?.children[6]?.innerHTML!);
               semester === 0
                 ? gradeArray[0]!.push({ value: grade, weight: weight })
@@ -57,14 +55,40 @@ class Grades {
           }
         }
         this.regularGrades[formattedSubject] = { name: subject, grades: gradeArray };
-      } else {
-        console.log("brak ocen lub ocena punktowa");
       }
     }
-    console.log(this.regularGrades["język_angielski"]);
+    console.log(this.regularGrades);
   }
 
-  getPointGrades() {}
+  getPointGrades() {
+    for (let i = 0; i < this.pointGradesTable!.length - 2; i += 2) {
+      const subject =
+        this.pointGradesTable![i]!.children[1]!.innerHTML.search("<") != -1
+          ? this.pointGradesTable![i]!.children[1]!.innerHTML.split("<")[0]!.trim()
+          : this.pointGradesTable![i]!.children[1]!.innerHTML!;
+      const formattedSubject = this.formatSubjectName(this.pointGradesTable![i]!.children[1]!.innerHTML);
+      if (this.pointGradesTable![i]!.children[2]!.innerHTML !== "Brak ocen") {
+        const grades = this.pointGradesTable![i + 1]!.children[1]!.children[0]!.children[1]!.children;
+        let semester = -1;
+        let gradeArray: [Grade[], Grade[]] = [[], []];
+        for (let j = 0; j < grades.length; j++) {
+          if (grades[j]!.innerHTML.includes("Okres")) {
+            semester++;
+          } else {
+            if (grades[j]?.children[0]?.innerHTML.search("/") !== -1 && grades[j]?.children[0]?.innerHTML !== "0/0") {
+              const grade = parseFloat(grades[j]?.children[0]?.innerHTML!.split("/")[0]!);
+              const weight = parseFloat(grades[j]?.children[0]?.innerHTML!.split("/")[1]!);
+              semester === 0
+                ? gradeArray[0]!.push({ value: grade, weight: weight })
+                : gradeArray[1]!.push({ value: grade, weight: weight });
+            }
+          }
+        }
+        this.pointGrades[formattedSubject] = { name: subject, grades: gradeArray };
+      }
+    }
+    console.log(this.pointGrades);
+  }
 
   formatSubjectName(name: string) {
     let formattedName = "";
